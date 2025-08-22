a6eb69ba01a4d7eefc00c3bf0e6fd9aca7d14a37
DmytroDD2 <vv676888@gmail.com>
2025-08-22 22:25:01 +0300

    fix frontend2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,11 @@ const nextConfig = {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname),
+      '@/lib': path.resolve(__dirname, 'lib'),
+      '@/components': path.resolve(__dirname, 'components'),
+      '@/hooks': path.resolve(__dirname, 'hooks'),
+      '@/types': path.resolve(__dirname, 'types'),
+      '@/app': path.resolve(__dirname, 'app'),
     }
     return config
   },
